c9785122415836fb11d5b0ef7ebcc0f6a680c9b5
Missaoui Raslen <149624976+raslenraslen@users.noreply.github.com>
2025-02-02 20:48:36 +0100

    Create ufwmaster.sh

---
+++ docs/kubernetes/ansible+k8s/ufwmaster.sh
@@ -0,0 +1,7 @@
+sudo firewall-cmd --permanent --add-port=6443/tcp
+sudo firewall-cmd --permanent --add-port=2379-2380/tcp
+sudo firewall-cmd --permanent --add-port=10250/tcp
+sudo firewall-cmd --permanent --add-port=10251/tcp
+sudo firewall-cmd --permanent --add-port=10252/tcp
+sudo firewall-cmd --permanent --add-port=10255/tcp
+sudo firewall-cmd --reloadCopied!
